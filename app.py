d108b8fa0c488d4889edeb22154870db2d22ef91
Destroyeur6315 <romain.fillot630@gmail.com>
2024-05-26 16:34:06 +0200

    :sparkles: new menu

--- app.py
+++ app.py
@@ -1,30 +1,21 @@
 import streamlit as st
+from menu import menu
 import pandas as pd
-import numpy as np
 from st_aggrid import AgGrid
 
-st.set_page_config(layout="wide")
+# Initialize st.session_state.role and st.session_state.role to None
+if "step" not in st.session_state:
+    st.session_state.step = None
+    st.session_state.dataset = None
 
-st.title('Web application interactive')
-
-df = pd.read_csv('netflix.csv')
-
-# Component de streamlit pour afficher dataset 
-AgGrid(df)
-
-# Résumé statistique 
-st.write(df.shape)
-st.write(df.columns, df.isnull().sum())
-
-# st.line_chart(df, y=['release year'])
-
-option = st.selectbox(
-     'What is your favorite color?',
-     ('Blue', 'Red', 'Green'))
-
-st.write('Your favorite color is ', option)
+def show_data():
+    st.subheader('Dataset')
+    AgGrid(st.session_state.dataset)
+    st.subheader('Descriptive Statistics')
+    st.write(st.session_state.dataset.describe())
 
 ### 1. Exploration des données
+## Upload CSV
 st.subheader('Input CSV')
 uploaded_file = st.file_uploader("Choose a file")
 
@@ -32,35 +23,13 @@ if uploaded_file is not None:
     file_extension = uploaded_file.name.split('.')[-1]
     if file_extension.lower() == 'csv':
         df = pd.read_csv(uploaded_file)
-        st.subheader('DataFrame')
-        st.write(df)
-        st.subheader('Descriptive Statistics')
-        st.write(df.describe())
+        st.session_state.step = "clean-data"
+        st.session_state.dataset = df
     else:
         st.error('☝️ Please upload a CSV file')
 
-st.sidebar.header('Setting')
+## Show dataset
+if st.session_state.dataset is not None:
+    show_data()
 
-col1, col2, col3 = st.columns(3)
-
-user_name = st.sidebar.text_input('What is your name?')
-user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
-user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
-
-with col1:
-  if user_name != '':
-    st.write(f'👋 Hello {user_name}!')
-  else:
-    st.write('👈  Please enter your **name**!')
-
-with col2:
-  if user_emoji != '':
-    st.write(f'{user_emoji} is your favorite **emoji**!')
-  else:
-    st.write('👈 Please choose an **emoji**!')
-
-with col3:
-  if user_food != '':
-    st.write(f'🍴 **{user_food}** is your favorite **food**!')
-  else:
-    st.write('👈 Please choose your favorite **food**!')
+menu()
