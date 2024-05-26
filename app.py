fe98923141c2fbab7ee52bcf2ba5adb66d4daf17
thdupin2 <theo.dupin@etu.uca.fr>
2024-05-26 16:58:14 +0200

    :sparkles:Add: suppression of missing values + replacement of missing values

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ st.write('Your favorite color is ', option)
 
 ### 1. Exploration des données
 st.subheader('Input CSV')
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a file", type="csv")
 
 if uploaded_file is not None:
     file_extension = uploaded_file.name.split('.')[-1]
@@ -64,3 +64,89 @@ with col3:
     st.write(f'🍴 **{user_food}** is your favorite **food**!')
   else:
     st.write('👈 Please choose your favorite **food**!')
+
+
+### 2. Data pre-processing and cleaning
+st.subheader('2. Data Pre-processing and Cleaning')
+def load_csv(file):
+    try:
+        df = pd.read_csv(file)
+        return df
+    except pd.errors.EmptyDataError:
+        st.error('Le fichier CSV est vide.')
+    except pd.errors.ParserError:
+        st.error('Erreur lors de la lecture du fichier CSV.')
+    except Exception as e:
+        st.error(f'Une erreur inattendue est survenue : {e}')
+    return None
+
+csv_file = st.file_uploader("Choose a CSV file to clean", type="csv")
+
+if csv_file is not None:
+    df = load_csv(csv_file)
+    if df is not None:
+      st.subheader('Original DataFrame')
+      AgGrid(df, key="original_df")
+
+      st.subheader('Summary Statistics')
+      st.write(df.describe())
+
+      st.subheader('Missing Values')
+      st.write(df.isnull().sum())
+
+      # Options to handle missing values
+      st.subheader('Handle Missing Values')
+      missing_value_action = st.radio(
+        "Choose how to handle missing values:",
+        ('Remove Rows', 'Remove Columns')
+      )
+
+      if missing_value_action == 'Remove Rows':
+        df_cleaned = df.dropna()
+        st.write("Rows with missing values removed")
+      if missing_value_action == 'Remove Columns':
+        df_cleaned = df.dropna(axis=1)
+        st.write("Columns with missing values removed")
+
+      st.subheader(f'DataFrame after handling missing values ({missing_value_action})')
+      AgGrid(df_cleaned, key="cleaned_df")  # Affiche le DataFrame nettoyé avec une clé unique
+
+      st.subheader(f'Summary Statistics after Cleaning ({missing_value_action})')
+      st.write(df_cleaned.describe())
+
+      st.subheader(f'Missing Values after Cleaning ({missing_value_action})')
+      st.write(df_cleaned.isnull().sum())
+
+      replace_option = st.selectbox(
+        'Choose an option to replace missing values',
+        ('Median', 'Mean', 'Mode')
+      )
+
+      if replace_option == 'Median':
+        ## Options to replace missing values with median
+        st.subheader('Replace Missing Values with Median')
+        numeric_columns = df.select_dtypes(include=np.number).columns
+        df_median = df.copy()
+        df_median[numeric_columns] = df_median[numeric_columns].fillna(df_median[numeric_columns].median())
+        st.write(df_median)
+      elif replace_option == 'Mean':
+        ## Options to replace missing values with mean
+        st.subheader('Replace Missing Values with Mean')
+        numeric_columns = df.select_dtypes(include=np.number).columns
+        df_mean = df.copy()
+        df_mean[numeric_columns] = df_mean[numeric_columns].fillna(df_mean[numeric_columns].mean())
+        st.write(df_mean)
+      else:
+        ## Options to replace missing values with mode of the column
+        st.subheader('Replace Missing Values with Mode')
+        not_numeric_columns = df.select_dtypes(exclude=np.number).columns
+        df_mode = df.copy()
+        df_mode[not_numeric_columns] = df_mode[not_numeric_columns].fillna(df_mode[not_numeric_columns].mode().iloc[0])
+        st.write(df_mode)
+
+      
+
+      
+
+      
+
