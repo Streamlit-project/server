d249e56ffa254b20320203f49f684a1b21659d61
thdupin2 <theo.dupin03300@gmail.com>
2024-06-11 16:58:47 +0200

    :construction: wip: data cleaning and replacement

--- front/main.py
+++ front/main.py
@@ -11,7 +11,7 @@ file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
 
-from back.main import load_csv
+from back.main import KNN, linearRegression, load_csv, mean, median, mode
 
 from menu import menu
 import pandas as pd
@@ -51,8 +51,6 @@ menu()
 ### 2. Data pre-processing and cleaning
 st.subheader('2. Data Pre-processing and Cleaning')
 
-
-
 csv_file = st.file_uploader("Choose a CSV file to clean", type="csv")
 
 if csv_file is not None:
@@ -65,7 +63,8 @@ if csv_file is not None:
       st.write(df.describe())
 
       st.subheader('Missing Values')
-      st.write(df.isnull().sum())
+      df = df.replace('unknown', None)
+      st.write(df.apply(lambda x: x.isin([None, np.nan]).sum()))
 
       st.header('Do you want to remove missing values ?')
       option = st.selectbox('Select yes or no', ('Yes', 'No'))
@@ -92,101 +91,43 @@ if csv_file is not None:
         st.write(df_cleaned.describe())
 
         st.subheader(f'Missing Values after Cleaning ({missing_value_action})')
-        st.write(df_cleaned.isnull().sum())
+        st.write(df_cleaned.apply(lambda x: x.isin(['unknown', None, np.nan]).sum()))
       else:
         # Si il ya des varaibles non numériques dans le dataset, faire un mode ou un kNN
         if(df.select_dtypes(exclude=np.number).shape[1] > 0):
           string_options = st.selectbox(
-            'Choose an option to replace non numeric missing values',
-            ('Mode', 'k-Nearest Neighbors')
+            'Do you want to replace non numeric missing values',
+            ('Yes, with Mode', 'No')
           )
 
-          # if(string_options == 'Mode'):
-          #   st.subheader('Replace Missing Values with Mode')
-          #   not_numeric_columns = df.select_dtypes(exclude=np.number).columns
-          #   df_mode = df.copy()
-          #   df_mode[not_numeric_columns] = df_mode[not_numeric_columns].fillna(df_mode[not_numeric_columns].mode().iloc[0])
-          #   st.write(df_mode)
-          # else:
+          if(string_options == 'Yes, with Mode'):
+            st.subheader('Replace Missing Values with Mode')
+            not_numeric_columns = df.select_dtypes(exclude=np.number).columns
+            df[not_numeric_columns] = df[not_numeric_columns].fillna(df[not_numeric_columns].mode().iloc[0])
+          else:
+            st.write('No action taken to replace non numeric missing values')
              
-
         replace_option = st.selectbox(
           'Choose an option to replace missing values',
-          ('Median', 'Mean', 'Mode', 'k-Nearest Neighbors', 'Linear Regression')
+          ('Median', 'Mean', 'k-Nearest Neighbors', 'Linear Regression')
         )
 
         if replace_option == 'Median':
           ## Options to replace missing values with median
           st.subheader('Replace Missing Values with Median')
-          numeric_columns = df.select_dtypes(include=np.number).columns
-          df_median = df.copy()
-          df_median[numeric_columns] = df_median[numeric_columns].fillna(df_median[numeric_columns].median())
+          df_median = median(df) 
           st.write(df_median)
         elif replace_option == 'Mean':
           ## Options to replace missing values with mean
           st.subheader('Replace Missing Values with Mean')
-          numeric_columns = df.select_dtypes(include=np.number).columns
-          df_mean = df.copy()
-          df_mean[numeric_columns] = df_mean[numeric_columns].fillna(df_mean[numeric_columns].mean())
+          df_mean = mean(df)
           st.write(df_mean)
-        elif replace_option == 'Mode':
-          ## Options to replace missing values with mode of the column
-          st.subheader('Replace Missing Values with Mode')
-          not_numeric_columns = df.select_dtypes(exclude=np.number).columns
-          df_mode = df.copy()
-          df_mode[not_numeric_columns] = df_mode[not_numeric_columns].fillna(df_mode[not_numeric_columns].mode().iloc[0])
-          st.write(df_mode)
         elif replace_option == 'k-Nearest Neighbors':
-          ## Impute missing values with k-Nearest Neighbors
           st.subheader('Replace Missing Values with k-Nearest Neighbors')
-
-          # Séparer les colonnes numériques et non numériques
-          numeric_cols = df.select_dtypes(include=['number']).columns
-          non_numeric_cols = df.select_dtypes(exclude=['number']).columns
-
-          numeric_data = df[numeric_cols]
-          non_numeric_data = df[non_numeric_cols]
-
-          # Imputer les valeurs manquantes uniquement sur les colonnes numériques
-          imputer = KNNImputer(n_neighbors=5)
-          numeric_data_imputed = pd.DataFrame(imputer.fit_transform(numeric_data), columns=numeric_cols)
-
-          # Réassembler le dataset avec les colonnes non numériques
-          data_imputed = pd.concat([numeric_data_imputed, non_numeric_data], axis=1)
-
-          # Afficher les données après imputation
+          data_imputed = KNN(df)
           st.write("Données après imputation des valeurs manquantes")
           st.write(data_imputed)
         else:
           st.subheader('Replace Missing Values with Linear Regression for Multiple Target Columns')
-
-          feature_columns = st.multiselect('Select feature columns for imputation', df.select_dtypes(include=['number']).columns)
-          target_columns = st.multiselect('Select target columns with missing values', df.select_dtypes(include=['number']).columns)
-
-          if len(target_columns) == 0:
-              st.warning("No target columns selected.")
-          elif len(feature_columns) == 0:
-              st.warning("No feature columns selected.")
-          else:
-              X = df[feature_columns]
-              y = df[target_columns]
-
-              imputer = SimpleImputer(strategy='mean')
-              regressor = LinearRegression()
-              pipeline = Pipeline([('imputer', imputer), ('regressor', regressor)])
-
-              X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-              # Appliquer l'imputation sur les colonnes cibles
-              for col in target_columns:
-                  if pd.isna(y[col]).any():  # Vérifier si des valeurs manquantes existent
-                      y_train_col = SimpleImputer(strategy='mean').fit_transform(y_train[col].values.reshape(-1, 1)).ravel()
-                      pipeline.fit(X_train, y_train_col)
-                      missing_indices = np.where(np.isnan(y[col]))[0]
-                      X_missing = X.iloc[missing_indices]
-                      y_pred = pipeline.predict(X_missing)
-                      df.loc[missing_indices, col] = y_pred
-                  else:
-                      st.warning(f"Aucune valeur manquante trouvée dans {col}. Pas besoin d'imputer.")
-
-              st.write(df)
+          df_linear = linearRegression(df)
+          st.write(df_linear)
