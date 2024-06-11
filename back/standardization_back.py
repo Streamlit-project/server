663a1ef1f807cd264c12198229774a93046a22ab
Lou <lou.valade@etu.uca.fr>
2024-06-11 17:22:11 +0200

    🚧 Keep string values to standardization

--- back/standardization_back.py
+++ back/standardization_back.py
@@ -9,18 +9,26 @@ def deleteNoneNumberColumns(df):
     numeric_columns = df.select_dtypes(include=np.number).columns
     return df[numeric_columns]
 
+def encode_string_columns(df):
+    dataFrame = df.copy()
+    le = pp.LabelEncoder()
+    for column in dataFrame.columns:
+        if dataFrame[column].dtype == 'object':
+            dataFrame[column] = le.fit_transform(dataFrame[column])
+    return dataFrame
+
 def min_max_standardization(df):
-    df = deleteNoneNumberColumns(df)
+    df = encode_string_columns(df)
     scaler = pp.MinMaxScaler()
     scaler.fit(df)
     return pd.DataFrame(scaler.transform(df), columns=df.columns)
 
 def z_score_standardization(df):
-    df = deleteNoneNumberColumns(df)
+    df = encode_string_columns(df)
     return (df - df.mean()) / df.std()
 
 def robust_standardization(df):
-    df = deleteNoneNumberColumns(df)
+    df = encode_string_columns(df)
     scaler = pp.RobustScaler()
     scaler.fit(df)
     return pd.DataFrame(scaler.transform(df), columns=df.columns)
