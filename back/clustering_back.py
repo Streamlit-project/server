bb98da9fc66b93c6f588e73320ffc8d275d1d27f
thdupin2 <theo.dupin03300@gmail.com>
2024-06-24 22:26:33 +0200

    :hammer: update: kMeans in clustering part

--- back/clustering_back.py
+++ back/clustering_back.py
@@ -4,16 +4,17 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 import streamlit as st
 
-def perform_pca_and_kmeans(data, n_clusters=3, init_method='k-means++', max_iter=300, n_init=10):
+def perform_pca_and_kmeans(data, n_clusters=3, n_components=None, init_method='k-means++', max_iter=300, n_init=10):
     # Vérifier la dimensionnalité des données
     n_samples, n_features = data.shape
-    
+
     if n_features < 2:
         st.error("Les données doivent avoir au moins deux attributs pour appliquer PCA avec 2 composantes principales.")
-    
+
     # Déterminer le nombre de composantes à utiliser pour PCA
-    n_components = min(n_samples, n_features)
-    
+    if n_components is None:
+        n_components = min(n_samples, n_features)
+
     # Appliquer PCA avec le nombre approprié de composantes
     pca = PCA(n_components=n_components)
     X = pca.fit_transform(data)
--- front/pages/clustering.py
+++ front/pages/clustering.py
@@ -17,11 +17,11 @@ df = st.session_state.dataset
 if df is not None:
     # Afficher un aperçu des données
     st.write("Aperçu des données :", df.head())
-    
+
     # Sélectionner les colonnes à utiliser pour le clustering
     numeric_columns = df.select_dtypes(include=np.number).columns.tolist()
     columns = st.multiselect("Sélectionner les colonnes à utiliser pour le clustering", numeric_columns)
-    
+
     if columns:
         # Sidebar pour les paramètres de l'algorithme
         n_clusters = st.sidebar.slider('Nombre de clusters (k)', 2, 10, 3, key='n_clusters')
@@ -29,13 +29,22 @@ if df is not None:
         max_iter = st.sidebar.slider('Nombre maximum d\'itérations', 100, 1000, 300, key='max_iter')
         n_init = st.sidebar.slider('Nombre d\'initialisations différentes', 1, 10, 10, key='n_init')
 
+        # Vérifier le nombre de colonnes sélectionnées
+        n_features = len(columns)
+
+        # Déterminer le nombre de composantes à utiliser pour PCA
+        if n_features < 3:
+            n_components = n_features
+        else:
+            n_components = 2
+
         # Appliquer le clustering K-means avec PCA en utilisant la fonction backend
         selected_data = df[columns].values
-        X, labels, centroids, original_centers, points_count = perform_pca_and_kmeans(selected_data, n_clusters=n_clusters, init_method=init_method, max_iter=max_iter, n_init=n_init)
-        
+        X, labels, centroids, original_centers, points_count = perform_pca_and_kmeans(selected_data, n_clusters=n_clusters, n_components=n_components, init_method=init_method, max_iter=max_iter, n_init=n_init)
+
         # Vérifier le nombre de composantes principales dans X
-        n_components = X.shape[1]   
         if n_components < 2:
+            st.write("Le nombre de composantes principales doit être supérieur ou égal à 2 pour afficher les résultats en 2D.")
             exit()
         else:
             # Affichage des résultats du clustering en 2D avec les centroids
@@ -49,9 +58,9 @@ if df is not None:
             ax.set_xlabel('Component 1')  # Légende de l'axe X
             ax.set_ylabel('Component 2')  # Légende de l'axe Y
             st.pyplot(fig)
-            
+
             st.write("Centroids des clusters:", centroids)
-            
+
             # Conversion des données en DataFrame pour un affichage sous forme de tableau
             clusters_data = {
                 'Cluster': [],
@@ -59,37 +68,31 @@ if df is not None:
                 'Dimension 2': [],
             }
 
-            for cluster, (dim1, dim2) in enumerate(points_count):
-                clusters_data['Cluster'].append(f"Cluster {cluster}")
-                clusters_data['Dimension 1'].append(dim1)
-                clusters_data['Dimension 2'].append(dim2)
+            for i in range(n_clusters):
+                clusters_data['Cluster'].append(f"Cluster {i}")
+                clusters_data['Dimension 1'].append(centroids[i][0])
+                clusters_data['Dimension 2'].append(centroids[i][1])
 
-            df = pd.DataFrame(clusters_data)
+            df = pd.DataFrame(data=clusters_data)
 
             # Affichage du DataFrame dans Streamlit
             st.write("Nombre de points dans chaque cluster :")
             st.table(df)
-            
-            # Créer un DataFrame à partir des données transformées par PCA (X) et des étiquettes de KMeans (labels)
-            df = pd.DataFrame(data=X, columns=['PCA '+str(i) for i in range(1, X.shape[1]+1)])
-            df['Cluster'] = labels
 
-            # Ajouter une explication pour les colonnes PCA et Cluster
-            st.write('Les colonnes "PCA 1", "PCA 2", etc. représentent les composantes principales des données. La colonne "Cluster" contient les étiquettes de KMeans.')
+            # Créer un DataFrame à partir des données transformées par PCA (X) et des étiquettes de KMeans (labels)
+            df = pd.DataFrame(data=X, columns=['PCA '+ str(i) for i in range(1, X.shape[1]+1)])
+            df['Cluster'] = labels
 
             # Afficher le DataFrame avec les composantes principales et les étiquettes de KMeans
             st.dataframe(df)
 
             # Si vous voulez ajouter les données originales, vous pouvez les concaténer avec le DataFrame existant
-            df_original = pd.DataFrame(data=selected_data, columns=['Colonne '+str(i) for i in range(1, selected_data.shape[1]+1)])
+            df_original = pd.DataFrame(data=selected_data, columns=[labels for labels in columns])
             df = pd.concat([df, df_original], axis=1)
-
-            # Ajouter une explication pour les colonnes des données originales
-            st.write('Les colonnes "Colonne 1", "Colonne 2", etc. représentent les valeurs originales des données avant la transformation PCA et le clustering KMeans.')
 
             # Afficher le DataFrame avec les composantes principales, les étiquettes de KMeans et les données originales
             st.dataframe(df)
-            
+
     else:
         st.write("Veuillez sélectionner les colonnes à utiliser pour le clustering.")
 else:
