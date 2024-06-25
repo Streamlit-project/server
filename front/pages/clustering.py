7cfbf95e4412ee7e4285449c340ad64e941db4f8
thdupin2 <theo.dupin03300@gmail.com>
2024-06-25 11:37:29 +0200

    :hammer: update: kMeans clustering

--- front/pages/clustering.py
+++ front/pages/clustering.py
@@ -1,9 +1,10 @@
-from matplotlib import pyplot as plt
+from matplotlib import colors, pyplot as plt
 import numpy as np
 import pandas as pd
 import streamlit as st
 from back.clustering_back import perform_pca_and_kmeans
 from front.menu import menu_with_redirect
+import plotly.graph_objects as go
 
 # Redirect to app.py if not logged in, otherwise show the navigation menu
 menu_with_redirect()
@@ -12,11 +13,11 @@ st.title('Clustering K-means')
 
 st.sidebar.header('Paramètres de l\'algorithme')
 
-df = st.session_state.dataset
+df = st.session_state.df_normalized
 
 if df is not None:
     # Afficher un aperçu des données
-    st.write("Aperçu des données :", df.head())
+    st.write("Aperçu des données :", df)
 
     # Sélectionner les colonnes à utiliser pour le clustering
     numeric_columns = df.select_dtypes(include=np.number).columns.tolist()
@@ -24,6 +25,7 @@ if df is not None:
 
     if columns:
         # Sidebar pour les paramètres de l'algorithme
+        n_components = st.sidebar.selectbox('Nombre de composantes principales', [2, 3], key='n_components')
         n_clusters = st.sidebar.slider('Nombre de clusters (k)', 2, 10, 3, key='n_clusters')
         init_method = st.sidebar.selectbox('Méthode d\'initialisation', ['k-means++', 'random'], key='init_method')
         max_iter = st.sidebar.slider('Nombre maximum d\'itérations', 100, 1000, 300, key='max_iter')
@@ -32,52 +34,74 @@ if df is not None:
         # Vérifier le nombre de colonnes sélectionnées
         n_features = len(columns)
 
-        # Déterminer le nombre de composantes à utiliser pour PCA
-        if n_features < 3:
-            n_components = n_features
-        else:
-            n_components = 2
-
         # Appliquer le clustering K-means avec PCA en utilisant la fonction backend
         selected_data = df[columns].values
-        X, labels, centroids, points_count = perform_pca_and_kmeans(selected_data, n_clusters=n_clusters, n_components=n_components, init_method=init_method, max_iter=max_iter, n_init=n_init)
+        n_features = selected_data.shape[1]
 
-        # Vérifier le nombre de composantes principales dans X
-        if n_components < 2:
-            st.write("Le nombre de composantes principales doit être supérieur ou égal à 2 pour afficher les résultats en 2D.")
-            exit()
+        if n_features < 2:
+            st.error("Veuillez sélectionner au moins deux colonnes pour appliquer la PCA.")
+        elif n_components > n_features:
+            st.error(f"Le nombre de composantes principales doit être inférieur ou égal au nombre de caractéristiques (nombres de composantes principales : {n_components}, nombre de caractéristiques : {n_features}).")
         else:
-            # Affichage des résultats du clustering en 2D avec les centroids
-            fig, ax = plt.subplots()
-            colors = ['red', 'blue', 'green', 'purple', 'orange', 'brown', 'pink', 'gray', 'olive', 'cyan']
-            for i in range(n_clusters):
-                points = X[labels == i]
-                ax.scatter(points[:, 0], points[:, 1], c=colors[i % len(colors)], label=f'Cluster {i}')
-            ax.scatter(centroids[:, 0], centroids[:, 1], s=100, c='yellow', marker='X', edgecolor='black', label='Centroids')
-            ax.legend()
-            ax.set_xlabel('Component 1')  # Légende de l'axe X
-            ax.set_ylabel('Component 2')  # Légende de l'axe Y
-            st.pyplot(fig)
+            X, labels, centroids, points_count = perform_pca_and_kmeans(selected_data, n_clusters=n_clusters, n_components=n_components, init_method=init_method, max_iter=max_iter, n_init=n_init)
 
-            st.write("Centroids des clusters:", centroids)
+            # Vérifier le nombre de composantes principales dans X
+            if n_components < 2:
+                st.write("Le nombre de composantes principales doit être supérieur ou égal à 2 pour afficher les résultats en 2D.")
+                exit()
+            else:
+                # Définir les couleurs à utiliser pour les clusters
+                colors = ['red', 'blue', 'green', 'purple', 'orange', 'brown', 'pink', 'gray', 'olive', 'cyan']
 
-            # Conversion des données en DataFrame pour un affichage sous forme de tableau
-            clusters_data = {
-                'Cluster': [],
-                'Dimension 1': [],
-                'Dimension 2': [],
-            }
+                # Affichage des résultats du clustering en 2D ou 3D avec les centroids
+                if n_components == 2:
+                    fig, ax = plt.subplots()
+                    for i in range(n_clusters):
+                        points = X[labels == i]
+                        ax.scatter(points[:, 0], points[:, 1], c=colors[i % len(colors)], label=f'Cluster {i}')
+                    ax.scatter(centroids[:, 0], centroids[:, 1], s=100, c='yellow', marker='X', edgecolor='black', label='Centroids')
+                    ax.legend()
+                    ax.set_xlabel('Component 1')
+                    ax.set_ylabel('Component 2')
+                    st.pyplot(fig)
+                else:
+                    fig = plt.figure()
+                    ax = fig.add_subplot(111, projection='3d')
+                    for i in range(n_clusters):
+                        points = X[labels == i]
+                        ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=colors[i % len(colors)], label=f'Cluster {i}')
+                    ax.scatter(centroids[:, 0], centroids[:, 1], centroids[:, 2], s=100, c='yellow', marker='X', edgecolor='black', label='Centroids')
+                    ax.legend()
+                    ax.set_xlabel('Component 1')
+                    ax.set_ylabel('Component 2')
+                    ax.set_zlabel('Component 3')
 
-            for i in range(n_clusters):
-                clusters_data['Cluster'].append(f"Cluster {i}")
-                clusters_data['Dimension 1'].append(centroids[i][0])
-                clusters_data['Dimension 2'].append(centroids[i][1])
+                    # Ajuster les marges du graphique pour éviter que les labels ne soient coupés
+                    fig.tight_layout()
+                    fig.subplots_adjust(bottom=0.5, left=0.5, right=2.5, top=2.5)
 
-            df = pd.DataFrame(data=clusters_data)
+                    st.pyplot(fig)
 
-            # Affichage du DataFrame dans Streamlit
-            st.write("Nombre de points dans chaque cluster :")
-            st.table(df)
+                # Conversion des données en DataFrame pour un affichage sous forme de tableau
+                clusters_data = {
+                    'Cluster': [],
+                }
+
+                for i in range(n_clusters):
+                    clusters_data['Cluster'].append(f"Cluster {i}")
+
+                for j in range(n_components):
+                    clusters_data[f'Component {j+1}'] = []
+
+                for i in range(n_clusters):
+                    for j in range(n_components):
+                        clusters_data[f'Component {j+1}'].append(centroids[i][j])
+
+                df = pd.DataFrame(data=clusters_data)
+
+                # Affichage du DataFrame dans Streamlit
+                st.write("Centroïds de chaque cluster :")
+                st.table(df)
 
     else:
         st.write("Veuillez sélectionner les colonnes à utiliser pour le clustering.")
